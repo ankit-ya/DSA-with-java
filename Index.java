c6ed1805cbb521cd02f86b921faa08a0d7ee4a32
ankit ya <ankit3001yadav@gmail.com>
2025-01-31 15:49:14 +0530

    trail2

--- Index.java
+++ Index.java
@@ -1,7 +1,8 @@
 public class Index{
     public static void main(String[] args){
         System.out.println(" Hello ankit ");
-        
+        System.out.println("Hello Ankit yadav");
+
 
     }
 }
